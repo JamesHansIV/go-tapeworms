a9c84fccb9bcd93e3a8fd584fb7fbf057ba17349
JamesHansIV <jameshanselmaniv@ku.edu>
2022-05-02 06:59:49 -0500

    added request file for client reqs to node server

---
+++ client/src/requests.js
@@ -0,0 +1,8 @@
+const fetchList = async () => {
+    console.log('fetchlist called');
+    await fetch('/order-list')
+        .then(response=>response.json())
+        .then(data=>console.log(data));
+}
+
+export default fetchList;
