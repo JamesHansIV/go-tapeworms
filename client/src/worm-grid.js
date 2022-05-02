221d39eb7283c45ce59863a1e97df732b115f89d
JamesHansIV <jameshanselmaniv@ku.edu>
2022-05-02 07:00:24 -0500

    added worm grid component

---
+++ client/src/worm-grid.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+class WormGrid extends React.Component {
+    state = {
+        error : null,
+        isLoaded : false,
+        worms: []
+    };
+
+    componentDidMount() {
+        fetch('/order-list')
+        .then(response=>response.json())
+        .then(data=> {
+                this.setState({
+                    isLoaded : true,
+                    worms : data
+                });
+            }, (error) => {
+                console.log("worm loading error: ",error);
+                this.setState({
+                    isLoaded: true,
+                    error
+                });
+            },
+        )
+    }
+
+    render() {
+        return (
+            <div>
+                {
+                    this.state.worms.map((worm) => (
+                        <li key={worm.id}>
+                            <div>
+                                <p>{worm.name}</p>
+                            </div>
+                        </li>
+                    ))
+                }
+            </div>
+        );
+    }
+}
+
+export default WormGrid;
