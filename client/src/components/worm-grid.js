56dd80826bf81ad5eba7b834c16c38a00e698bb4
JamesHansIV <jameshanselmaniv@ku.edu>
2022-05-05 08:24:57 -0500

    switched to hook from class

--- client/src/components/worm-grid.js
+++ client/src/components/worm-grid.js
@@ -1,52 +1,47 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import '../App.css';
 
-class WormGrid extends React.Component {
-    state = {
-        error : null,
-        isLoaded : false,
-        worms: []
-    };
+function WormGrid() {
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [worms, setWorms] = useState([]);
 
-    componentDidMount() {
-        fetch('/order-list')
-        .then(response=>response.json())
-        .then(data=> {
-                this.setState({
-                    isLoaded : true,
-                    worms : data
-                });
-            }, (error) => {
-                console.log("worm loading error: ",error);
-                this.setState({
-                    isLoaded: true,
-                    error
-                });
+    useEffect(() => {
+        fetchWorms();
+    }, [])
+
+    const fetchWorms = async () => {
+        await fetch('/order-list')
+            .then(response=>response.json())
+            .then(data=> {
+                setIsLoaded(true);
+                setWorms(data);
+            }, (err) => {
+                console.log("worm loading error: ",err);
+                setIsLoaded(true);
+                setError(err)
             },
         )
     }
-
     
 
-    render() {
-        return (
-            <div className='worm-grid' >
-                {
-                    this.state.worms.map((worm) => (
-                        <div key={worm.id}>
-                            <p>{"./src/img/" + worm.img_src}</p>
-                            <div>
-                                {worm.name}<br/>
-                                <img url={"./src/img/" + worm.img_src}/>
+    return (
+        <div className='worm-grid' >
+            {
+                worms.map((worm) => (
+                    <div key={worm.id}>
+                        <p>{"./src/img/" + worm.img_src}</p>
+                        <div>
+                            {worm.name}<br/>
+                            <img url={"./src/img/" + worm.img_src}/>
 
-                            </div>
-                            {/*<img src={'/img/'+ worm.img_src}></img> */}
                         </div>
-                    ))
-                }
-            </div>
-        );
-    }
+                        {/*<img src={'/img/'+ worm.img_src}></img> */}
+                    </div>
+                ))
+            }
+        </div>
+    );
 }
 
 export default WormGrid;
