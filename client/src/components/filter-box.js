5cc0c9d61314054922fba6849e82179ae5ed69b7
JamesHansIV <jameshanselmaniv@ku.edu>
2022-05-05 08:08:30 -0500

    changed component structure to hook. session data

--- client/src/components/filter-box.js
+++ client/src/components/filter-box.js
@@ -1,56 +1,73 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../App.css';
-import ReactDOM from 'react-dom/client';
+// import ReactDOM from 'react-dom/client';
 
-class FilterBox extends React.Component {
-    state = {
-        minLength: 0,
-        maxLength: 25,
-        minTestes: 0,
-        maxTestes: 65,
-        minBothria: 0,
-        maxBothria: 5
+
+
+function FilterBox(props) {
+    const [minLength, setMinLength] = useState(0);
+    const [maxLength, setMaxLength] = useState(25);
+    const [minTestes, setMinTestes] = useState(0);
+    const [maxTestes, setMaxTestes] = useState(65);
+    const [scolexFt, setScolexFt] = useState('');
+    const [parasiteOf, setParasite] = useState('');
+    const [hasApOrg, setHasApOrg] = useState(null);
+
+    const handleApplyFilter = () => {
+        //clear filters
+        sessionStorage.clear();
+
+        //convert filters to json
+        const filters = {
+            minLength: minLength,
+            maxLength: maxLength,
+            minTestes: minTestes,
+            maxTestes: maxTestes,
+            scolexFt: scolexFt,
+            parasiteOf: parasiteOf,
+            hasApOrg: hasApOrg
+        }
+
+        //submit filters to session storage
+        sessionStorage.setItem('filters',JSON.stringify(filters));
     }
 
-    render() {
-        return (
-            <div>
-                <label>&#8195;Length 
-                    <input id='length-min' type='numeric' value={this.state.minLength}/>
-                    <input id='length-max' type='numeric' value={this.state.maxLength}/>
-                </label><br/><br/>
+    return (
+        <div>
+            <label>&#8195;Length 
+                <input id='length-min' type='numeric' value={minLength} onChange={(e) => setMinLength(e.target.value)}/>
+                <input id='length-max' type='numeric' value={maxLength} onChange={(e) => setMaxLength(e.target.value)}/>
+            </label><br/><br/>
 
-                <label>&#8195;Testes 
-                    <input id='testes-min' type='numeric' value={this.state.minTestes}/>
-                    <input id='testes-max' type='numeric' value={this.state.maxTestes}/>
-                </label><br/><br/>
+            <label>&#8195;Testes 
+                <input id='testes-min' type='numeric' value={minTestes} onChange={(e) => setMinTestes(e.target.value)}/>
+                <input id='testes-max' type='numeric' value={maxTestes} onChange={(e) => setMaxTestes(e.target.value)}/>
+            </label><br/><br/>
 
-                &#8195;Scolex Features<br/>
-                &#8195;&#8195;<input id='bothria-radio' type='radio' name='scolex-feature' value='bothria'/><label for='bothria-radio'>Bothria</label>
-                &#8195;&#8195;<input id='bothridia-radio' type='radio' name='scolex-feature' value='bothridia'/><label for='bothridia-radio'>Bothridia</label>
-                &#8195;&#8195;<input id='suckers-radio' type='radio' name='scolex-feature' value='suckers'/><label for='suckers-radio'>Suckers</label>
-                &#8195;&#8195;<input id='scolex-unkown-radio' type='radio' name='scolex-feature' value='unkown'/><label for='scolex-unkown-radio'>Unkown</label>
-                <br/><br/>
+            &#8195;Scolex Features<br/>
+            &#8195;&#8195;<input id='bothria-radio' type='radio' name='scolex-feature' value='bothria' onChange={e=>setScolexFt(e.target.value)}/><label htmlFor='bothria-radio'>Bothria</label>
+            &#8195;&#8195;<input id='bothridia-radio' type='radio' name='scolex-feature' value='bothridia' onChange={e=>setScolexFt(e.target.value)}/><label htmlFor='bothridia-radio'>Bothridia</label>
+            &#8195;&#8195;<input id='suckers-radio' type='radio' name='scolex-feature' value='suckers' onChange={e=>setScolexFt(e.target.value)}/><label htmlFor='suckers-radio'>Suckers</label>
+            &#8195;&#8195;<input id='scolex-unkown-radio' type='radio' name='scolex-feature' value='unkown' onChange={e=>setScolexFt(e.target.value)}/><label htmlFor='scolex-unkown-radio'>Unkown</label>
+            <br/><br/>
 
-                &#8195;Parasite of...<br/>
-                &#8195;&#8195;<input id='stingrays-radio' type='radio' name='parasite-of' value='stingrays'/><label for='stingrays-radio'>Stingrays</label>
-                &#8195;&#8195;<input id='eagle-rays-radio' type='radio' name='parasite-of' value='eagle-rays'/><label for='eagle-rays-radio'>Eagle Rays</label>
-                &#8195;&#8195;<input id='sharks-radio' type='radio' name='parasite-of' value='sharks'/><label for='sharks-radio'>Sharks</label>
-                &#8195;&#8195;<input id='parasite-unkown-radio' type='radio' name='parasite-of' value='unkown'/><label for='parasite-unkown-radio'>Unkown</label>
-                <br/><br/>
+            &#8195;Parasite of...<br/>
+            &#8195;&#8195;<input id='stingrays-radio' type='radio' name='parasite-of' value='stingrays' onChange={e=>setParasite(e.target.value)}/><label htmlFor='stingrays-radio'>Stingrays</label>
+            &#8195;&#8195;<input id='eagle-rays-radio' type='radio' name='parasite-of' value='eagle-rays' onChange={e=>setParasite(e.target.value)}/><label htmlFor='eagle-rays-radio'>Eagle Rays</label>
+            &#8195;&#8195;<input id='sharks-radio' type='radio' name='parasite-of' value='sharks' onChange={e=>setParasite(e.target.value)}/><label htmlFor='sharks-radio'>Sharks</label>
+            &#8195;&#8195;<input id='parasite-unkown-radio' type='radio' name='parasite-of' value='unkown' onChange={e=>setParasite(e.target.value)}/><label htmlFor='parasite-unkown-radio'>Unkown</label>
+            <br/><br/>
 
-                &#8195;Apical Organ<br/>
-                &#8195;&#8195;<input id='has-apical-radio' type='radio' name='has-apical' value='true'/><label for='has-apical-radio'>Present</label>
-                &#8195;&#8195;<input id='no-apical-radio' type='radio' name='has-apical' value='false'/><label for='no-apical-radio'>Not Present</label>
-                <br/><br/>
+            &#8195;Apical Organ<br/>
+            &#8195;&#8195;<input id='has-apical-radio' type='radio' name='has-apical' value='true' onChange={e=>setHasApOrg(e.target.value)}/><label htmlFor='has-apical-radio'>Present</label>
+            &#8195;&#8195;<input id='no-apical-radio' type='radio' name='has-apical' value='false' onChange={e=>setHasApOrg(e.target.value)}/><label htmlFor='no-apical-radio'>Not Present</label>
+            <br/><br/>
 
-                &#8195;<input id='update-btn' type='button' value='Update Filter'/>
-                &#8195;<input id='reset-btn' type='button' value='Reset Filter'/>
-            </div>
-            
-        )
-    }
-    
+            &#8195;<input id='apply-btn' type='button' value='Apply Filter' onClick={handleApplyFilter}/>
+            &#8195;<input id='reset-btn' type='button' value='Reset Filter'/>
+        </div>
+    )
+
 }
 
 export default FilterBox;
