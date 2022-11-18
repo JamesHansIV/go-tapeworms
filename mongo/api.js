cfa7f18b02b43cf97ef9d993fe5756c7996a1fab
JamesHansIV <jameshanselmaniv@ku.edu>
2022-11-18 09:27:24 -0600

    updated api

--- mongo/api.js
+++ mongo/api.js
@@ -25,7 +25,7 @@ routes.route("/worms/lecanicephalidea").get(async function(req, res) {
         });
 });
 
-routes.route("/worms/:genus/:species").get(async function(req, res) {
+routes.route("/worms/:order/:genus").get(async function(req, res) {
     const connection = dbo.getDb();
     const params = req.params;
 
@@ -33,10 +33,10 @@ routes.route("/worms/:genus/:species").get(async function(req, res) {
 
     // query db
     connection
-        .collection(params.genus)
-        .find({name: params.species})
+        .collection(params.order)
+        .find({name: params.genus})
         .toArray(function (err, result) {
-            if (err) res.status(400).send(`Error fetching ${params.species} from ${params.genus}`);
+            if (err) res.status(400).send(`Error fetching ${params.genus} from ${params.order}`);
             else res.json(result);
         });
 })
@@ -84,49 +84,26 @@ routes.route("/close").get(async function() {
 
 routes.route("/update/").get( async function(req, res) {
     const connection = dbo.getDb();
-    let { genus = "lecanicephalidea", _species,val} = req.query;
+    let { genus = "lecanicephalidea", _genus, _apolysis, _field, _val} = req.query;
 
-    console.log(req.query, _species, val)
-    // connection.collection("lecanicephalidea").updateMany({},[{$set:{name: {$toLower: "$name"}}}]);
+    console.log(req.query)
 
-    // connection
-    //     .collection("lecanicephalidea")
-    //     .updateMany(
-    //         {},
-    //         [   
-    //             { $set : { "host" : {
-    //                 $switch : {
-    //                     branches : [
-    //                         { case: { $eq: ["$batoid_shark", "shark"]}, then: ["shark"]},
-    //                         { case: { $eq: ["$batoid_shark", "batoid"]}, then: ["batoid"]},
-    //                         { case: { $eq: ["$batoid_shark", "batoid/shark"]}, then: ["shark","batoid"]}
-    //                     ]
-    //                 }
-    //             }}
-    //             }, { $unset: "batoid_shark"}
-                
-    //         ]
+    if (_genus) { _genus = _genus.toLowerCase(); }
+    if (_apolysis && typeof(_apolysis)==String) {_apolysis = _apolysis.toLowerCase();}
+    if (_val && typeof(_val)==String) {_val = _val.toLowerCase();}
+
+    console.log(_genus, _apolysis, _val)
+
+    // connection.collection("lecanicephalidea").updateOne(
+    //     { name: { $eq: _genus } },
+    //     // { $push: { apolysis: _apolysis } },
+    //     // { $push: { $proglottids_margins: _proglottids_margins }}
+    //     { $push: { proglottids_margins: _val, }}
     // );
 
-    connection.collection("lecanicephalidea").updateOne(
-        { name: { $eq: _species } },
-        { $push: { _apolysis: val } }
-        // [ 
-        //     { $set : { "apolysis" : {
-        //         $switch : {
-        //             branches : [
-        //                 { case: { $eq: ["$apolysis", "euapolytic"]}, then: ["euapolytic"]},
-        //                 { case: { $eq: ["$apolysis", "apolytic"]}, then: ["apolytic"]},
-        //                 { case: { $eq: ["$apolysis", "hyperapolytic/euapolytic"]}, then: ["euapolytic","hyperapolytic"]},
-        //                 { case: { $eq: ["$apolysis", "euapolytic/apolytic"]}, then: ["shark","batoid","apolytic"]}
-        //             ], 
-        //             default: "No Match"
-        //         }
-        //     }}}
-        // ]
-    );
+    connection.collection("lecanicephalidea").updateMany({},{ $rename: {"Laterally_expanded_immature_proglottids": "laterally_expanded_immature_proglottids"}});
 
-    res.send("success");
+    res.send(`_genus: ${_genus}  <br>  _field: ${_field}  <br>  _val: ${_val}`);
 
 })
 
