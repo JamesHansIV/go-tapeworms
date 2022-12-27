fd3afdb9b31d166c934dfbe72bf440dae5c2519e
James Hanselman <jameshanselmaniv@ku.edu>
2022-12-27 12:44:20 -0600

    updated server_test default port

--- mongo/server_test.js
+++ mongo/server_test.js
@@ -2,7 +2,7 @@ const express = require ("express");
 const cors = require("cors");
 const dbo = require("./connect");
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 const app = express();
 
 app.use(cors());
