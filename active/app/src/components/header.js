f6925d5dae7880a8465a0d60bac30b3c7de06b05
James Hanselman <jameshanselmaniv@ku.edu>
2023-01-11 18:54:22 -0600

    created header component

---
+++ active/app/src/components/header.js
@@ -0,0 +1,23 @@
+import React, {useEffect, useState} from "react";
+import { BrowserRouter } from "react-router-dom";
+
+import NavBar from "./nav-bar.js";
+import styles from "./header.module.css";
+
+function Header() {
+    return (
+        <div className={ styles.container }>
+            <div className={ styles.titleContainer }>
+                <h1 className={ styles.title }>Tapeworms Unlocked</h1>
+                <h3 className={ styles.subtitle }>Interactive Identification Keys to Tapeworms</h3>
+            </div>
+            <div className={ styles.navContainer }>
+                <BrowserRouter>
+                    <NavBar/>
+                </BrowserRouter>
+            </div>
+        </div>
+    );
+}
+
+export default Header;  
