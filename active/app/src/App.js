8cf4f9719ba53ab0c773ad4daee77561abe3bb3d
James Hanselman <jameshanselmaniv@ku.edu>
2023-01-13 21:42:51 -0600

    added worm grid to app body

--- active/app/src/App.js
+++ active/app/src/App.js
@@ -2,16 +2,25 @@ import logo from './logo.svg';
 
 // components
 import Header from './components/header.js';
+import WormGrid from './components/worm-grid';
+
+import img from './images/Seussapex_KA206Asc2nd.jpg';
+
 
 function App() {
+
   return (
     <div className="App">
       <Header/>
       <div style={{
-        backgroundColor: "red",
-        height: 1000
-      }}
-      />
+        backgroundColor: "white",
+        height: 200
+      }} /> 
+
+      <div>
+        <WormGrid/>
+        {/* <img src={img} alt="import failed"/> */}
+      </div>
     </div>
   );
 }
