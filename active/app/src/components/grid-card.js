0eadb14cf5c13d375dc846fcf06b895c35bcbe2f
James Hanselman <jameshanselmaniv@ku.edu>
2023-02-03 21:59:49 -0600

    updated grid card to display images from dynamic source

--- active/app/src/components/grid-card.js
+++ active/app/src/components/grid-card.js
@@ -2,27 +2,33 @@ import React, {useState} from 'react';
 
 import styles from './grid-card.module.css';
 
-
-import img from '../images/Seussapex_KA206Asc2nd.jpg';
-// import img from '../images/test.jpg';
-
 function GridCard(props) {
     const [genus, setGenus] = useState(props.genus);
 
     return (
-        <div className={styles.container}>
+        props.loading == true ? (
+            <div className={styles.container}>
+                <div className={styles.portrait}>
+                    <div className={styles.skeleton}/>
+                </div>
+
+                <div className={styles.nameTagSkeleton}/>
+            </div>
+        ) : (
+            <div className={styles.container}> 
             {/* Image (carousel?) */}
+                <div className={styles.portrait}>
+                    <img 
+                        className={styles.image}
+                        src={`${process.env.PUBLIC_URL}/images/${props.img}`} alt="Cannot Find Image :("
+                    />
+                </div>
 
-            <div className={styles.portrait}>
-                <img 
-                    className={styles.image}
-                    src={img} alt="number 2"/>
+                <div className={styles.nameTag}>
+                    {genus}
+                </div>
             </div>
-
-            <div className={styles.nameTag}>
-                {genus}
-            </div>
-        </div>
+        )
     );
 }
 
