b233da8d042a810a9cf08cddff59d673952d0146
James Hanselman <jameshanselmaniv@ku.edu>
2023-02-22 10:40:01 -0600

    created detailed-feature-selection component

---
+++ active/app/src/components/detailed-feature-selection.js
@@ -0,0 +1,59 @@
+import React, {useState, useEffect} from 'react';
+
+import styles from './detailed-feature-selection.module.css';
+import FeatureSelectorModal from './feature-selector-modal';
+import RadioPillSelector from './radio-pill-selector';
+
+function DetailedFeatureSelection (props) {
+
+    const [selectionExpanded, setSelectionExpanded] = useState(false);
+    const [modalActive, setModalActive] = useState(false);
+
+    // useEffect()
+
+    const toggleExpansion = () => setSelectionExpanded(!selectionExpanded);
+    const toggleModal = () => setModalActive(!modalActive);
+
+
+    return (
+        <div className={styles.container}>
+            <div className={styles.titleContainer}>
+                <h5 className={styles.title}>
+                    {props.title}
+                    
+                    {/* <p>?</p> */}
+                </h5>
+                <button className={styles.button} onClick={toggleExpansion}>
+                    {selectionExpanded ? "Hide" : "Show"}
+                </button>
+                <button className={styles.button} onClick={toggleModal}>
+                    Info
+                </button>
+            </div>
+            
+            
+                <div>
+                    { selectionExpanded && 
+                        <RadioPillSelector 
+                            inputDict={props.inputDict}
+                            value={props.value}
+                            setValue={props.setValue}
+                            orientation={'vertical'}
+                        />
+                    }
+                    { modalActive &&
+                        <FeatureSelectorModal
+                            src={['test','test','test']}
+                            inputDict={props.inputDict}
+                            value={props.value}
+                            setValue={props.setValue}
+                            active={true}
+                        />                    
+                    }
+                </div>
+
+        </div>
+    )
+}
+
+export default DetailedFeatureSelection;
