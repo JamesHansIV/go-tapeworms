5e172e562415c73524ec801120726a8883987d00
ElenBhattarai <elenbhattarai2002@ku.edu>
2023-04-30 01:21:50 -0500

    used react widget DropdownList

--- active/app/src/components/suggestion-text-box.js
+++ active/app/src/components/suggestion-text-box.js
@@ -1,43 +1,26 @@
 import React, {useState} from 'react';
-import './suggestion-text-box.css'
+import DropdownList from "react-widgets/DropdownList";
+import "react-widgets/styles.css";
 
 export const SuggestionTextBox = (props) => {
     const options = props.options
     const heading = props.heading
-    const [value, setValue] = useState("");
-    const [showOptions, setShowOptions] = useState(false)
-    const [suggestions, setSuggestions]= useState([])
+    const [selected, setSelected] = useState(null);
 
-    const handleTextChange = (e) => {
-        const current_text = e.target.value;
-        setValue(current_text)
-        setShowOptions(current_text.length > 0 ? true : false)
-        setSuggestions(
-            options.filter((option) => option.toLowerCase().includes(current_text.toLowerCase()))
-        )
+    const handleDropdownChange = (value) => {
+        setSelected(value);
     }
 
-    const handleSelectSuggestion = (suggestion) => {
-        setValue(suggestion);
-        setShowSuggestions(false);
-    };
-
     return (
-        <>
-        <div className='body'>
-            <label className='label-heading'>{heading}</label><br/>
-            <input type = "text" value = {value} onChange={handleTextChange}/>
-            {showSuggestions && (
-                <ul>
-                    {suggestions.map((suggestion) => (
-                        <li key={suggestion} onClick={() => handleSelectSuggestion(suggestion)}>
-                        {suggestion}
-                        </li>
-                    ))}
-                </ul>
-            )}
-        </div>
-        </>
+        <div>
+        <h3>{heading}</h3>
+        <DropdownList
+            defaultValue="Select a host genera"
+            data ={options}
+            value={selected}
+            onChange={handleDropdownChange}
+        />
+      </div>
     )
 }
 
