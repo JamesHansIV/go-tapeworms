0827f731f7fec1190a3f269ee5806fce4c848f40
ElenBhattarai <elenbhattarai2002@ku.edu>
2023-04-30 02:28:14 -0500

    fixed the condition if the user wants to select nothing after selecting something

--- active/app/src/components/suggestion-text-box.js
+++ active/app/src/components/suggestion-text-box.js
@@ -3,19 +3,23 @@ import DropdownList from "react-widgets/DropdownList";
 import "react-widgets/styles.css";
 
 export const SuggestionTextBox = (props) => {
-    const options = props.options
+    const options = ['----None----', ...props.options];
     const heading = props.heading
     const [selected, setSelected] = useState(null);
 
     const handleDropdownChange = (value) => {
-        setSelected(value);
+        if(value == '----None----'){
+            setSelected(null)
+            value = ''
+        } else {
+            setSelected(value)
+        }
     }
 
     return (
         <div>
         <h3>{heading}</h3>
         <DropdownList
-            defaultValue="Select a host genera"
             data ={options}
             value={selected}
             onChange={handleDropdownChange}
