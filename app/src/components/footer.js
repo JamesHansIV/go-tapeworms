61d824626966ef32788aa69fafe3969de08cacca
James Hanselman <68711757+JamesHansIV@users.noreply.github.com>
2024-12-13 12:37:48 -0600

    updated credits in filter

--- app/src/components/footer.js
+++ app/src/components/footer.js
@@ -11,12 +11,12 @@ function Footer () {
             <div className={styles.grantContainer}>
                 <img className={styles.nsfLogo} src={"nsf_logo.png"}/>
                 <p className={styles.text}>NSF DEB Award<br/>Nos. 1921404 & 1921411</p>
-                <a className={styles.text} href="/citation-guidelines">Project & Citation Information</a>
+                {/* <a className={styles.text} href="/citation-guidelines">Project & Citation Information</a> */}
             </div>
             <div className={styles.infoContainer}>
                 <img className={styles.siteLogo} src={"logo.png"}/>
                 <p className={styles.copyright}>Copyright © 2024 Tapeworms Unlocked</p>
-                <p className={styles.text}>Created by J. Hanselman & K. Jensen &nbsp;
+                <p className={styles.text}>Created by J. Hanselman, J. N. Caira & K. Jensen &nbsp;
                     {/* <a className={styles.text} href="/">CONTACT US</a>     */}
                     {/* replace href with /contact */}
                 </p>
